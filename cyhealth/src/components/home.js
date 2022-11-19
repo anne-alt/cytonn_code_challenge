abee1a0e2e5c7fc9aab72f83a72788360a09b09c
Anne Ogongo <anneogongo98@gmail.com>
2022-11-19 18:33:05 +0300

    Fixed Key

--- cyhealth/src/components/home.js
+++ cyhealth/src/components/home.js
@@ -3,25 +3,25 @@ import React, {useState, useEffect} from "react";
 function Home() {
     const [data, setData] = useState([]);
 
+
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'baddc226acmshf0a25cef37c726bp16aa86jsnd78faf82c85a',
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
             'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
         }
     };
 
     useEffect( () => {
     
-    fetch('https://covid-193.p.rapidapi.com/statistics/',options)
+    fetch('https://covid-193.p.rapidapi.com/statistics/', options)
         .then(response => response.json())
-        .then(data => console.log(data.response))
+        .then(data => console.log(data))
         .catch(err => console.error(err));
 }, []);
 return (
     <div>{data}</div>
-)
-}
+)}
 
 export default Home;
 
